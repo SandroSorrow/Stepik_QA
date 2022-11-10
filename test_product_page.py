3dcfd2d6240239a7eeb87adbf1f7d9c9465045eb
SandroSorrow <Sandro.Sorrow@gmail.com>
2022-11-11 01:22:40 +0300

    bug fix

--- test_product_page.py
+++ test_product_page.py
@@ -37,20 +37,22 @@ class TestUserAddToBasketFromProductPage:
         page.should_be_correct_product_price(product_price)
 
 
+# @pytest.mark.parametrize('link', [
+#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+#     pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+#                  marks=pytest.mark.xfail),
+#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+#     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9",
+# ])
 @pytest.mark.need_review
 @pytest.mark.parametrize('link', [
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                 marks=pytest.mark.xfail),
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9",
-])
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"])
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
@@ -62,7 +64,6 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_correct_product_price(product_price)
 
 
-@pytest.mark.need_review
 @pytest.mark.parametrize('link', [
     pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                  marks=pytest.mark.xfail),
@@ -168,6 +169,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser, link):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', [
     "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/",
 ])
